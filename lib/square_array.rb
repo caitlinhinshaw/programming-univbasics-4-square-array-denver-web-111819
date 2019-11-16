d4ed68b2a1ba7a51c80d59b850d2e661f8cfe194
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-16 01:40:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
   array_squared = []
   index = 0
-  
+
   while counter < array.length do
     array_squared << ((array[index])**2)
     index++
   end
-  
+
   array_squared
 end
