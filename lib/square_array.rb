d7f5a3822d18c7011b7e99fd5bf2c82236a5cb7c
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-16 01:44:20 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  array_squared = []
+  index = 0
+
+  while index < array.length do
+    array_squared << ((array[index])**2)
+    index += 1
+  end
+
+  array_squared
+  
 end
